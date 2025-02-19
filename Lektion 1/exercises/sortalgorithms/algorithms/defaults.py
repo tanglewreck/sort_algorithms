1a9afddb32cf595708e7cfad6e3c336dffd7567b
mier <mier@tanglewreck.mier.org>
2025-02-19 20:24:48 +0100

    Updated defaults

--- Lektion 1/exercises/sortalgorithms/algorithms/defaults.py
+++ Lektion 1/exercises/sortalgorithms/algorithms/defaults.py
@@ -7,13 +7,13 @@ __all__ = ["ITERATIONS", "TIMEIT_ITERATIONS"]
 __all__ += ["N", "MIN", "MAX"]
 
 # Number of numbers in a list
-N = 100
+N = 1000
 # Min size of a number
 MIN = 1
 # Max size of a number
 MAX = 1000
 
 # Number of iterations while measuring performance
-ITERATIONS = 100
+ITERATIONS = 10
 # Number of iterations using the timeite module
-TIMEIT_ITERATIONS = 100
+TIMEIT_ITERATIONS = 10
