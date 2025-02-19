2cfc02bb8b8a1998c8a1b079bc79e2f186209802
mier <mier@orion>
2025-02-19 22:02:51 +0100

    Added an outer loop and code to collect data for each run of the loop, in the end calculating the average of the collected data (for now, only for timeit execution times but in the end for all collected statistics and also standard deviations

--- Lektion 1/exercises/sortalgorithms/main.py
+++ Lektion 1/exercises/sortalgorithms/main.py
@@ -19,62 +19,82 @@ def main() -> None:
     and average number of swaps of bubbleSort() and
     mySortAlgorithm()"""
 
-    # Initialise three lists used for collecting
-    # performance data of the algorithms. Later used
+    # Initialise three lists used for collecting 
+    # performance data of each run. Later used
     # (see below) for comparing the two algorithms.
-    executionTimes = []
-    averageComparisons = []
-    averageSwaps = []
+    executionTimesCollected_mySortAlgorithm = []
+    executionTimesCollected_bubbleSort = []
 
-    # Once for each algorithm, 
-    # measure the performance
-    for algorithm in (mySortAlgorithm, bubbleSort):
+    averageComparisonsCollected = []
+    averageSwapsCollected = []
 
-        sortAlgorithm = algorithm
-        print(f"Measuring performance of {sortAlgorithm.__name__}")
-        print(f"List-length: {N}")
-        # Measure execution time
-        print(f"Execution time (using the timeit module) "
-              f"({TIMEIT_ITERATIONS} iterations):")
-        elapsed = timeIt(algorithm=sortAlgorithm)
-        executionTimes.append(elapsed)
-        print(f"Elapsed = %6.4f seconds ({TIMEIT_ITERATIONS} iterations)" % elapsed)
-        # Measure performance in terms of (mean) number of
-        # swaps and (mean) number of comparisons
-        print(f"Number of comparisons "
-              f"and swaps ({ITERATIONS} iterations):")
-        (averageNumberOfSwaps,
-         averageNumberOfComparisons) = algorithmPerformance(
-                                            iterations=ITERATIONS,
-                                            func=sortAlgorithm
-        )
-        # Print results
-        print(f"Average number of swaps: %6.4f" % averageNumberOfSwaps)
-        print(f"Average number of comparisons: {averageNumberOfComparisons}")
-        print("Done.")
-        print("")
+    # Loop and collect data
+    loopCount = 2
+    for _ in range(loopCount):
+
+        # Initialise three lists used for collecting
+        # performance data of the algorithms. Later used
+        # (see below) for comparing the two algorithms.
+        executionTimes = []
+        averageComparisons = []
+        averageSwaps = []
     
-        averageComparisons.append(averageNumberOfComparisons)
-        averageSwaps.append(averageNumberOfSwaps)
-        # Sort and display once to make verify that the algorithm works
-        ## randomNumbers = generateRandomList(MIN, MAX, N)
-        # Sort the list and collect statistics
-        ## (sortedRandomNumbers, _, _) = mySortAlgorithm(randomNumbers)
-        ## print(f"in = {randomNumbers}")
-        ## print(f"out = {sortedRandomNumbers}")
+        # Once for each algorithm, 
+        # measure the performance
+        for algorithm in (mySortAlgorithm, bubbleSort):
+    
+            sortAlgorithm = algorithm
+            print(f"Measuring performance of {sortAlgorithm.__name__}")
+            print(f"List-length: {N}")
+            # Measure execution time
+            print(f"Execution time (using the timeit module) "
+                  f"({TIMEIT_ITERATIONS} iterations):")
+            elapsed = timeIt(algorithm=sortAlgorithm)
+            executionTimes.append(elapsed)
+            print(f"Elapsed = %6.4f seconds ({TIMEIT_ITERATIONS} iterations)" % elapsed)
+            # Measure performance in terms of (mean) number of
+            # swaps and (mean) number of comparisons
+            print(f"Number of comparisons "
+                  f"and swaps ({ITERATIONS} iterations):")
+            (averageNumberOfSwaps,
+             averageNumberOfComparisons) = algorithmPerformance(
+                                                iterations=ITERATIONS,
+                                                func=sortAlgorithm
+            )
+            # Print results
+            print(f"Average number of swaps: %6.4f" % averageNumberOfSwaps)
+            print(f"Average number of comparisons: {averageNumberOfComparisons}")
+            print("Done.")
+            print("")
+        
+            averageComparisons.append(averageNumberOfComparisons)
+            averageSwaps.append(averageNumberOfSwaps)
+            # Sort and display once to make verify that the algorithm works
+            ## randomNumbers = generateRandomList(MIN, MAX, N)
+            # Sort the list and collect statistics
+            ## (sortedRandomNumbers, _, _) = mySortAlgorithm(randomNumbers)
+            ## print(f"in = {randomNumbers}")
+            ## print(f"out = {sortedRandomNumbers}")
+    
+    
+        executionTimesCollected_mySortAlgorithm.append(
+            executionTimes[0])
+        executionTimesCollected_bubbleSort.append(
+            executionTimes[1])
+        print("= " * 40)
+        print(f"Execution time ratio "
+              "(bubbleSort / mySortAlgorithm): %6.4f" % 
+              (executionTimes[1] / executionTimes[0]))
+        print(f"Average number of comparisons ratio "
+              "(bubbleSort / mySortAlgorithm): %6.4f" % 
+              (averageComparisons[1] / averageComparisons[0]))
+        print("Average number of swaps ratio "
+              "(bubbleSort / mySortAlgorithm): %6.4f" % 
+              (averageSwaps[1] / averageSwaps[0]))
+        print("= " * 40)
 
-
-    print("= " * 40)
-    print(f"Execution time ratio "
-          "(bubbleSort / mySortAlgorithm): %6.4f" % 
-          (executionTimes[1] / executionTimes[0]))
-    print(f"Average number of comparisons ratio "
-          "(bubbleSort / mySortAlgorithm): %6.4f" % 
-          (averageComparisons[1] / averageComparisons[0]))
-    print("Average number of swaps ratio "
-          "(bubbleSort / mySortAlgorithm): %6.4f" % 
-          (averageSwaps[1] / averageSwaps[0]))
-    print("= " * 40)
+    print("%6.4f" % (sum(executionTimesCollected_mySortAlgorithm) / loopCount))
+    print("%6.4f" % (sum(executionTimesCollected_bubbleSort) / loopCount))
 
 if __name__ == "__main__":
     main()
