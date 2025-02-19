116a5fc53f0d335e1d42abe3cdeb4edbc8fb9c8a
mier <mier@orion>
2025-02-19 19:37:28 +0100

    Added 'debug_msg()' et al. to algorithms.utils

--- Lektion 1/exercises/sortalgorithms/algorithms/utils.py
+++ Lektion 1/exercises/sortalgorithms/algorithms/utils.py
@@ -3,8 +3,12 @@
 Utilities 
 """
 
-__all__ = ["generateRandomList"]
+__all__ = ["generateRandomList",
+           "debug_msg",
+           "err_msg",
+           "sys_msg"]
 
+import inspect
 import sys
 import textwrap
 
@@ -78,3 +82,35 @@ def getCommandLineArguments(minimum: int = MIN,
         print(f"min, max, numbersToGenerate = {minimum}, {maximum}, {numbersToGenerate}")
 
     return (minimum, maximum, numbersToGenerate)
+
+
+def debug_msg(*args, end="\n"):
+    """Utility function. Prints debugging info on stderr"""
+    args_str = [str(x) for x in args]
+    msg = ' '.join(args_str)
+    caller = inspect.stack()[1].function
+    if caller == "<module>":
+        caller = "main"
+    sys.stderr.write(f"({caller}) {msg}{end}")
+
+
+def err_msg(*args, end="\n"):
+    """Utility function. Prints a(n error) message on stderr"""
+    args_str = [str(x) for x in args]
+    msg = ' '.join(args_str)
+    caller = inspect.stack()[1].function
+    if caller == "<module>":
+        caller = "main"
+    sys.stderr.write(f"({caller}) {msg}{end}")
+
+
+def sys_msg(*args, end="\n"):
+    """Utility function. Prints a message on stdout"""
+    args_str = [str(x) for x in args]
+    msg = ' '.join(args_str)
+    caller = inspect.stack()[1].function
+    if caller == "<module>":
+        caller = "main"
+    sys.stdout.write(f"({caller}) {msg}{end}")
+
+
