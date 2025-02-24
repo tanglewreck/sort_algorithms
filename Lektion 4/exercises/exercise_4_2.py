cf897ed4f499d32ea8297bc5334fd69e643090b4
mier <mier@localhost>
2025-02-24 15:50:25 +0100

    Stuff

--- Lektion 4/exercises/exercise_4_2.py
+++ Lektion 4/exercises/exercise_4_2.py
@@ -1,18 +1,45 @@
+"""exercise_4.2 doc string"""
 class Shape:
+    """Class doc string"""
     def __init__(self, color):
+        """__init__ doc string"""
         self.color = color
 
+
     def display_color(self):
+        """Method doc string"""
         print(f"This shape is {self.color}.")
 
+
+    def __str__(self):
+        """Return string representation of the object"""
+        return str(self.color)
+
+
 class Circle(Shape):
+    """Class doc string"""
     def __init__(self, color, radius):
+        """Method doc string"""
         super().__init__(color)
         self.radius = radius
 
     def display_radius(self):
+        """Method doc string"""
         print(f"The radius of this circle is {self.radius}.")
 
-circle = Circle("Red", 5)
-circle.display_color()
-circle.display_radius()
+    def __str__(self):
+        """Return string representation of the object"""
+        return f"{__doc__} {str(self.color)}, {str(self.radius)}"
+
+
+
+def main():
+    """main"""
+    circle = Circle("Red", 5)
+    circle.display_color()
+    circle.display_radius()
+    print(str(circle))
+
+
+if __name__ == "__main__":
+    main()
--- Lektion 4/exercises/exercise_4_3.py
+++ Lektion 4/exercises/exercise_4_3.py
@@ -1,13 +1,67 @@
-class Animal:
-    def __init__(self, name):
-        self.name = name
+"""exercise_4.3 doc string"""
 
-    def make_sound(self):
-        print(f"{self.name} says {self.sound}")
+class Animal:
+    """Class doc string"""
+    def __init__(self, name = "Generic animal") -> None:
+        """Method doc string"""
+        self.name = name
+        self.sound = "nothing"
+        self.class_name = "Animal"
+
+
+    def make_sound(self) -> str:
+        """Method doc string"""
+        # print(f"{self.name} says {self.sound}")
+        return self.sound
+
+
+    def __str__(self) -> str:
+        """Method doc string"""
+        if self.class_name[0] in ['A', 'E', 'I', 'O', 'U', 'Y']:
+            article = "An"
+        else:
+            article = "A"
+        return f"{article} {self.class_name} says '{self.sound}'"
+
 
 class Dog(Animal):
-    def __init__(self, name):
-        self.sound = "Woof"
+    """Class doc string"""
+    def __init__(self, name: str = None ) -> None:
+        """Method doc string"""
+        super().__init__(name)
+        self.sound = "woof"
+        self.class_name = "Dog"
 
-dog = Dog("Buddy")
-dog.make_sound()
+
+class Cat(Animal):
+    """Class doc string"""
+    def __init__(self, name: str = None) -> None:
+        super().__init__(name)
+        self.sound = "meow"
+        self.class_name = "Cat"
+
+
+def main() -> None:
+    """function doc string"""
+    # Print what (a generic) animal of each class says
+    print(str(Animal()))
+    print(str(Dog()))
+    print(str(Cat()))
+    print()
+
+    # Create an animal object and print what it says
+    animal = Animal()
+    print(f"{animal.name} says '{animal.make_sound()}'")
+
+    # Create a dog object and print what it says
+    dog = Dog("Buddy")
+    print(f"{dog.name} says '{dog.make_sound()}'")
+
+    # Create a cat object and print what it says
+    cat = Cat("Pussy")
+    print(f"{cat.name} says '{cat.make_sound()}'")
+
+
+
+if __name__ == "__main__":
+    main()
