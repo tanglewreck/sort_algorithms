24dfdbd92858fbb2b473967fac2fc53f2311a3ff
mier <mier@orion>
2025-02-25 08:12:05 +0100

    Stuff

--- Lektion 4/exercises/exercise_4_4.py
+++ Lektion 4/exercises/exercise_4_4.py
@@ -1,18 +1,49 @@
+"""
+    exercise_4.4.py
+    Date: 2025-02-25
+"""
 class Flower:
+    """Class docstring"""
     def __init__(self, name, color):
+        """Method docstring"""
         self.name = name
         self.color = color
+        self.species = "generisk blomma"
 
     def display_info(self):
+        """Method docstring"""
         print(f"{self.name} är en {self.color} blomma.")
 
+
+    def __str__(self):
+        return f"{self.name}: {self.color}"
+
+
 class Rose(Flower):
-    def __init__(self, fragrance, name, color):
+    """Class docstring"""
+    def __init__(self, name, color, fragrance):
+        """Method docstring"""
         super().__init__(name, color)
         self.fragrance = fragrance
+        self.species = "ros"
+
 
     def display_info(self):
-        print(f"{self.name} är en {self.color} ros som doftar {self.fragrance}.")
+        """Method docstring"""
+        print(f"{self.name} är en {self.color} {self.species} som doftar {self.fragrance}.")
+
+
+    def __add__(self, other):
+        """Method docstring"""
+        return f"{self.name} + {other.name}"
+
+
+    def __str__(self):
+        return f"{self.name} är en {self.color} {self.species} som doftar {self.fragrance}"
+
 
 rose = Rose("Ros", "röd", "sött")
-rose.display_info()
+maskros = Flower("maskros", "gul")
+
+print(rose)
+print(rose + maskros)
