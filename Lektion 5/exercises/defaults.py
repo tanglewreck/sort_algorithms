b37de289e0842bd5df869c249e62dc9fd856dfb8
mier <mier@localhost>
2025-03-05 13:46:14 +0100

    Widget defaults merged into a dictionary. Added a function, save_json(), to save defaults to a json file

--- Lektion 5/exercises/defaults.py
+++ Lektion 5/exercises/defaults.py
@@ -1,10 +1,9 @@
 """Default values for widgets"""
 
+import json
+
 __all__ = ["ROOT_GEOMETRY",
-           "DEFAULTS_BUTTON",
-           "DEFAULTS_CONTENTS",
-           "DEFAULTS_LABEL",
-           "DEFAULTS_TEXT"]
+           "DEFAULTS"]
 
 
 # Root window geometry
@@ -13,29 +12,24 @@ R_WIDTH = 800
 R_X = 100
 R_Y = 20
 ROOT_GEOMETRY = f"{R_WIDTH}x{R_HEIGHT}+{R_X}+{R_Y}"
-DEFAULTS_BUTTON = {
-    "padding": 10,
-    "width": 20
+DEFAULTS = {
+        "Button":   {"padding": 10, "width": 20},
+        "Label":    {"anchor": 'center', "border": 5,
+                     "borderwidth": 2, "justify": 'right',
+                     "padding": "10 10 10 10", "relief": "raised",
+                     "underline": 2, "width": 10},
+        "Contents": {"padding": 100},
+        "Text":     {"background": "#aaaaaa", "foreground": "#002055",
+                     "height": 8, "width": 30}
 }
+def save_json():
+    """Save defaults to file in json format"""
+    # defaults_json = json.dumps(DEFAULTS)
+    try:
+        with open("defaults.json", "w", encoding="utf8") as file_descriptor:
+            json.dump(DEFAULTS, file_descriptor)
+    except OSError as exception:
+        print(f"Error: {exception}")
 
-DEFAULTS_LABEL = {
-    "anchor": 'center',
-    "border": 5,
-    "borderwidth": 2,
-    "justify": 'right',
-    "padding": "10 10 10 10",
-    "relief": "raised", 
-    "underline": 2,
-    "width": 10
-}
-
-DEFAULTS_CONTENTS = {
-    "padding": 100
-}
-
-DEFAULTS_TEXT = {
-    "background": "#aaaaaa",
-    "foreground": "#002055",
-    "height": 8,
-    "width": 30
-}
+if __name__ == "__main__":
+    save_json()
