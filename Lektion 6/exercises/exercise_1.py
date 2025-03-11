cf3d801064ecc304b1e47c2d3abe71eef3edfebb
mier.misa <mier.misa@>
2025-03-11 15:19:17 +0100

    Stuff

--- Lektion 6/exercises/exercise_1.py
+++ Lektion 6/exercises/exercise_1.py
@@ -1,2 +1,30 @@
-# Skriv ett Python-program som skapar en textfil "test.txt"
-# och skriver in i den "Här står min testtext."
+"""
+= = = = = =
+Exercise 1
+= = = = = =
+"""
+
+import os
+import utils 
+from defaults import DEFAULTS
+
+__all__ = ['main', '__doc__']
+
+def main():
+    print(__doc__)
+    file_name = DEFAULTS['files']['fileName1']
+    text = DEFAULTS['texts']['text1']
+    try:
+        with open(file_name, 'w', encoding="utf8") as file:
+            file.write(f"{text}\n")
+            print("Successfully wrote the text") 
+            print(f"{text:>20s}")
+            print(f"to the file {file_name}")
+        utils.print_file_contents(file_name)
+    except OSError as exception:
+        print(f"OSError: {exception}")
+        raise
+
+
+if __name__ == "__main__":
+    print(__doc__)
--- Lektion 6/exercises/exercise_3.py
+++ Lektion 6/exercises/exercise_3.py
@@ -1,2 +1,36 @@
-# Skapa ett program som frågar efter användarens namn.
-# Lägg in namnet på slutet av filen "test.txt".
+"""
+= = = = = =
+Exercise 3
+= = = = = =
+"""
+
+import os
+import utils
+from defaults import DEFAULTS
+
+__all__ = ['main', '__doc__']
+
+def main():
+    print(__doc__)
+    file_name = DEFAULTS['files']['fileName1']
+    text = DEFAULTS['texts']['text1']
+    name = None
+    try:
+        try:
+            while not name:
+                name = input("Name, please: ")
+        except EOFError:
+            pass
+        with open(file_name, 'a', encoding="utf8") as file:
+            file.write(f"{name}\n")
+            print("Successfully appended the text") 
+            print(f"{name:>20s}")
+            print(f"to the file {file_name}")
+        utils.print_file_contents(file_name)
+    except OSError as exception:
+        print(f"OSError: {exception}")
+        raise
+
+if __name__ == "__main__":
+    print(__doc__)
+    main()
