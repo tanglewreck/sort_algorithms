b1c444d76d001461f43bf2c33a45a8c3597a0f6e
mier.misa <mier.misa@>
2025-03-31 14:17:19 +0200

    Removed commented code

--- Lektion 6/exercises/main.py
+++ Lektion 6/exercises/main.py
@@ -15,35 +15,6 @@ def main():
     except KeyError as exception:
         print(f"Got a KeyError (somewhere): {repr(exception)} ")
 
-###7import exercise_1
-###7import exercise_3
-###7import exercise_4
-###7import exercise_5
-###7import exercise_6
-###7import exercise_7
-###7import exercise_8
-###7import exercise_9
-###def main():
-###    """main:"""
-###    try:
-###        functions = [exercise_1,
-###                     exercise_3,
-###                     exercise_4,
-###                     exercise_5,
-###                     exercise_6,
-###                     exercise_7,
-###                     exercise_8,
-###                     exercise_9
-###                     ]
-###        for func in functions:
-###            func.main()
-###    # if function does not exist:
-###    except NameError as exception:
-###        print(repr(exception))
-###    # handle permission error
-###    except OSError as exception:
-###        print(f"OSError {exception}")
-
 
 if __name__ == "__main__":
     main()
