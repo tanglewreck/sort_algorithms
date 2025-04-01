3179b42b094ac5fc421346f9d80b3b39d42c275d
mier.misa <mier.misa@>
2025-04-01 13:49:37 +0200

    Stuff from lesson 9

--- Lektion 8/exercises/exercises_8.py
+++ Lektion 8/exercises/exercises_8.py
@@ -1,9 +1,15 @@
 """exercise lesson 8"""
 
 import random
+from tkinter import Button
 from tkinter import Canvas
+from tkinter import Label
+from tkinter import StringVar
 from tkinter import Tk
 
+
+# Initialise list (of balls)
+BALLS = []
 
 # The size of the Canvas widget implicitly determines
 # the size of the root widget:
@@ -17,16 +23,26 @@ COLOURS = ["red",
 # delay in ms until a ball is moved (again)
 DELAY = 50
 # Max and min speed in both x and y directions
-MAX_SPEED = 1
+MAX_SPEED = 3
 MIN_SPEED = 1
 # Number of balls to create
-NUMBER_OF_BALLS = 10
+NUMBER_OF_BALLS = 5
+
 
 
 class Ball:
     """Ball class"""
+    # Class variable to keep count of
+    # number of ball objects created.
+    number_of_balls = 0
+    # Number of clicks counter
+    number_of_clicks = 0
+
     def __init__(self, canvas, colour, radius):
         """constructor"""
+        # Increase ball count
+        # Ball.number_of_balls += 1
+        self.__class__.number_of_balls += 1
         self.canvas = canvas
         self.radius = radius
         self.colour = colour
@@ -47,6 +63,9 @@ class Ball:
 
     def change_colour(self, _ = None):
         """Change ball colour, randomly"""
+        # Update number of clicks counter
+        self.__class__.number_of_clicks += 1
+        print(self.__class__.number_of_clicks)
         # If the ball is blue, reverse
         if self.colour == "blue":
             self.reverse()
@@ -56,12 +75,21 @@ class Ball:
         #
         if self.colour == "yellow":
             new_colour = "green"
-        elif self.colour == "green":
+        # If the ball is green and there's more than
+        # one ball still left, delete the ball (i.e.
+        # delete the canvas oval that is the visual 
+        # representation of the ball; the ball object 
+        # is not deleted).
+        elif self.colour == "green" and \
+             self.__class__.number_of_balls > 1:
             # Make pylint not complain about 'Possibly
             # using varable "new_colour" before assignment' by assigning some
             # arbitrary value... (not really necessary, code wise):
             new_colour = None
+            # Delete the ball/oval
             self.canvas.delete(self.ball)
+            # Decrease ball count
+            self.__class__.number_of_balls -= 1
         else:
             new_colour = random.choice(COLOURS)
         self.canvas.itemconfig(self.ball, fill=new_colour)
@@ -92,10 +120,8 @@ class Ball:
         except ValueError:
             # If the ball/oval has been deleted, a ValueError
             # is raised the next time self.move() is called, so
-            # we catch the error and ignore it:
+            # we catch these errors and ignore them:
             pass
-
-
 
 
 def main():
@@ -103,29 +129,40 @@ def main():
 
     # root widget
     root = Tk()
-    root.title("Bollklickspelet")
+    root.title("balls")
 
     # Create a canvas where the balls can move around
     canvas = Canvas(root, width=CANVAS_WIDTH, height=CANVAS_HEIGHT, bg="white")
     canvas.pack()
 
-    # Initialise list (of balls)
-    balls = []
 
     # Add balls to the list
     ball_radius = 20
     # NB: This is a logical error in the
-    # original code
+    # original code (the colour should be "blue")
     # ball_colour = "#1122FF"
     for _ in range(NUMBER_OF_BALLS):
         ball_colour = random.choice(COLOURS)
-        balls.append(Ball(canvas, ball_colour, ball_radius))
+        BALLS.append(Ball(canvas, ball_colour, ball_radius))
 
 
     # Set the balls in motion
-    for ball in balls:
+    for ball in BALLS:
         ball.move()
 
+    # Add a counter label
+    counter_var = StringVar()
+    counter_var.set(str(Ball.number_of_clicks))
+    counter_label = Label(root, textvariable=counter_var)
+    counter_label.after(1000, 
+                        )
+    counter_label.pack()
+
+    # Add a Quit-button
+    quit_button = Button(root,
+                         text="Quit",
+                         command=quit)
+    quit_button.pack()
     # Execute the mainloop
     root.mainloop()
 
