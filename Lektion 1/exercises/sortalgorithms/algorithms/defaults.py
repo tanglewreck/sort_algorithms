0bd35fb336717744f97d3e9e28b96f5cc901bb97
mier <mier@localhost>
2025-04-15 12:35:10 +0200

    Updated defaults

--- Lektion 1/exercises/sortalgorithms/algorithms/defaults.py
+++ Lektion 1/exercises/sortalgorithms/algorithms/defaults.py
@@ -7,7 +7,7 @@ __all__ = ["ITERATIONS", "TIMEIT_ITERATIONS"]
 __all__ += ["N", "MIN", "MAX"]
 
 # Number of numbers in a list
-N = 200
+N = 10
 # Min size of a number
 MIN = 1
 # Max size of a number
@@ -17,6 +17,6 @@ MAX = 100
 ITERATIONS = 100
 # Number of iterations using the timeite module
 TIMEIT_ITERATIONS = 100
-# Number of measurements (the number of 
+# Number of measurements (the number of
 # times to run each test)
-MEASUREMENTS = 30
+MEASUREMENTS = 10
