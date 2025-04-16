aa43cf6bd8c3d7d7a7ee757c16c8a8292b80453e
mier <mier@localhost>
2025-04-16 12:06:27 +0200

    Updated defaults

--- Lektion 1/exercises/sortalgorithms/algorithms/defaults.py
+++ Lektion 1/exercises/sortalgorithms/algorithms/defaults.py
@@ -4,19 +4,19 @@ Defaults for sort algorithms and
 performance measurements.
 """
 __all__ = ["ITERATIONS", "TIMEIT_ITERATIONS"]
-__all__ += ["N", "MIN", "MAX"]
+__all__ += ["LIST_LENGTH", "MIN", "MAX"]
 
 # Number of numbers in a list
-N = 10
+LIST_LENGTH = 10
 # Min size of a number
 MIN = 1
 # Max size of a number
 MAX = 100
 
 # Number of iterations while measuring performance
-ITERATIONS = 100
+ITERATIONS = 10
 # Number of iterations using the timeite module
-TIMEIT_ITERATIONS = 100
+TIMEIT_ITERATIONS = 1000
 # Number of measurements (the number of
 # times to run each test)
-MEASUREMENTS = 10
+MEASUREMENTS = 100
