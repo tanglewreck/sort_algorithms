17066c72e8d971e54960c64718da47549eb1a069
mier <mier@tanglewreck.mier.org>
2025-05-02 15:02:53 +0200

    More work on collecting data into pandas DataFrame's

--- Lektion 1/exercises/sortalgorithms/sortperf.py
+++ Lektion 1/exercises/sortalgorithms/sortperf.py
@@ -32,8 +32,12 @@ from algorithms.defaults import TIMEIT_REPEAT
 
 from algorithms.performance import do_measurements
 from algorithms.performance import algorithm_perf
+from algorithms.performance import algo_perf
+from algorithms.performance import time_it_full
 from algorithms.plot import do_plots
 
+from algorithms.bubblesort import bubblesort
+from algorithms.bubblesortplus import bubblesort_plus
 
 def main() -> None:
     """
@@ -50,96 +54,195 @@ def main() -> None:
         print(algo.__name__)
     print()
 
-    # Initialise data-structure:
-    #     data -> {function}
-    #                 |-> {name/category}
-    #                          |-> {list-length}
-    #                                    |-> {'mean', 'stddev'}
-    data = {}
-    categories = ['elapsed', 'comp', 'swaps']
+#    def measure_elpased(algo, L = LIST_LENGTHS, iterations = 1, n = 5):
+#    """Create and return 'time measurement' data"""
+#    dfs = []
+#    for length in L:
+#        for _ in range(iterations):
+#            d = {"algo": np.repeat(algo, n),
+#                  "L": np.repeat(length, n),
+#                  "t": np.random.random(n) * 10 + 1}
+#            dfs.append(DataFrame(d, columns=["algo", "L", "t"]))
+#    return pd.concat(dfs, ignore_index=True)
+
+    dfs = []  # list of DataFrame's
     for algo in ALGORITHMS:
-        data[algo] = {'algorithm': algo.__name__}
-        for category in categories:
-            data[algo][category] = {'name': category}
-            for list_length in LIST_LENGTHS:
-                data[algo][category][list_length] = {'mean': 0.0,
-                                                     'stddev': 0.0}
 
-    # Collect data (means and stddevs for each data category)
-    for list_length in LIST_LENGTHS:
-        print(f"Measuring list-length {list_length}", end="...")
-        for algo in ALGORITHMS:
-            (data[algo]['elapsed'][list_length],
-             data[algo]['comp'][list_length],
-             data[algo]['swaps'][list_length]) = \
-                     do_measurements(algo,
-                                     list_length,
-                                     iterations=ITERATIONS,
-                                     timeit_repeat=TIMEIT_REPEAT,
-                                     timeit_iterations=TIMEIT_ITERATIONS)
-        print("done")
-    print()
-    time.sleep(1)
+        for length in LIST_LENGTHS:
+            # Initialise data dict
+            elapsed_data = {'algorithm': np.repeat(algo.__name__,
+                                                   TIMEIT_REPEAT),
+                            'length': np.repeat(length,
+                                                TIMEIT_REPEAT)}
 
-    # Collect data into a dictionary consisting of equal length
-    # lists for algorithm, list-length, elapsed, comps, and swaps
-    perf_data = {}
+            # Measure execution time (time_it_full() returns
+            # a list of TIMEIT_REPEAT floats (execution times ))
+
+            elapsed = np.array(time_it_full(algorithm=algo,
+                                   timeit_repeat=TIMEIT_REPEAT,
+                                   timeit_iterations=TIMEIT_ITERATIONS,
+                                   list_length=length))
+            # Update data dict
+            elapsed_data['elapsed'] = elapsed
+            # Make a DataFrame from the data dict
+            df = DataFrame(elapsed_data,
+                           columns=["algorithm", "length", "elapsed"])
+
+            # index=np.repeat(algo.__name__, TIMEIT_REPEAT),
+            # Append the dataframe to the list of dataframes
+            dfs.append(df)
+
+    # Concatenate the list of dataframes into one
+    df = pd.concat(dfs, ignore_index=True)
+    #print("df:")
+    #print(df)
+
+    # Collect averages
+    # algo = "bubblesort"
+    # elapsed_means = [df.loc[(df.algorithm==algo) &
+    #                          (df.length==L)].elapsed.mean()
+    #                  for L in df.loc[(df.algorithm==algo)].length]
+    # print([df.loc[(df.algorithm==algo) &
+    #               (df.length==L)].elapsed.mean()
+    #               for L in LIST_LENGTHS])
+
+    # Create a dataframe for averages
+    dfs = []
     for algo in ALGORITHMS:
-        for list_length in LIST_LENGTHS:
-            (elapsed, comps, swaps) = algorithm_perf(algo, list_length)
+        elapsed_means = np.zeros(len(LIST_LENGTHS))
+        for k, length in enumerate(LIST_LENGTHS):
+            elapsed_means[k] = df.loc[(df.algorithm==algo.__name__) &
+                                      (df.length==length)].elapsed.mean()
+        elapsed_data = {'algorithm': algo.__name__,
+                        'means': list(zip(LIST_LENGTHS, elapsed_means))}
 
-            # Append
-            perf_data.setdefault('algorithm', []).append(algo.__name__)
-            perf_data.setdefault('list_length', []).append(list_length)
-            perf_data.setdefault('elapsed', []).append(elapsed)
-            perf_data.setdefault('comps', []).append(comps)
-            perf_data.setdefault('swaps', []).append(swaps)
-    np.set_printoptions(precision=4, suppress=True)
-    
-    # Create a dataframe from the performance data
-    df = DataFrame(perf_data,
-                           columns=["algorithm",
-                                    "list_length",
-                                   "elapsed",
-                                   "comps",
-                                   "swaps"])
-    print("df:")
-    print(df)
-    # print()
-    # print(df["algorithm"])
-    # print("bubblesort elapsed")
-    # print(df.loc[df.algorithm == "bubblesort", "elapsed"])
-    # print()
-    for algo in ALGORITHMS:
-        print(f"algorithm: {algo.__name__}")
-        for list_length in LIST_LENGTHS:
-            d = df.loc[df.algorithm == algo.__name__].loc[
-                       df.list_length == list_length,
-                       "comps"]
-            print(f"comps mean ({list_length}): "
-                  f"{d.mean()} ± {d.std()}")
-        print()
+        elapsed_data_2 = DataFrame({'algorithm': algo.__name__,
+                          'length': LIST_LENGTHS,
+                          'means': [ df.loc[(df.algorithm==algo.__name__) &
+                                            (df.length==length)].elapsed.mean()
+                                     for length in LIST_LENGTHS]})
+        dfs.append(elapsed_data_2)
+    df_means = pd.concat(dfs, ignore_index=True)
 
-    # Print results
-    # for algo in ALGORITHMS:
-    for algo in data:
-        print("=" * 40, sep="")
-        print(f"algorithm: {data[algo]['algorithm']}")
-        print("=" * 40, sep="")
-        for list_length in LIST_LENGTHS:
-            print(f"list length: {list_length}")
-            for category in categories:
-                print(f"\t{category}: "
-                      f"{data[algo][category][list_length]['mean']:6.4f} "
-                      f"± {data[algo][category][list_length]['stddev']:6.4f}")
-            print("-" * 40, sep="")
-        print("\n\n")
-
-    # Plot results
-    do_plots(data, categories)
-
-    return data
-
-
+    # Plot means
+    plt.plot(LIST_LENGTHS, [df_means.loc[df_means.length==L].means for L in LIST_LENGTHS])
+    plt.legend([algo.__name__ for algo in ALGORITHMS])
+    plt.ylabel("elapsed (ms)")
+    plt.xlabel("list lengths")
+    plt.show()
+    # print(df_means)
+    # elapsed_data
+    # print(elapsed_means)
+    # breakpoint()
+    # print(df[df["length"] == 10]])
+    # print(df.loc[(df.algorithm == "bubblesort_plus") & (df.length == 10)].elapsed)
+#     quit()
+#
+#     # Initialise data-structure:
+#     #     data -> {function}
+#     #                 |-> {name/category}
+#     #                          |-> {list-length}
+#     #                                    |-> {'mean', 'stddev'}
+#     data = {}
+#     categories = ['elapsed', 'comp', 'swaps']
+#     for algo in ALGORITHMS:
+#         data[algo] = {'algorithm': algo.__name__}
+#         for category in categories:
+#             data[algo][category] = {'name': category}
+#             for list_length in LIST_LENGTHS:
+#                 data[algo][category][list_length] = {'mean': 0.0,
+#                                                      'stddev': 0.0}
+#
+#     # Collect data (means and stddevs for each data category)
+#     for list_length in LIST_LENGTHS:
+#         print(f"Measuring list-length {list_length}", end="...")
+#         for algo in ALGORITHMS:
+#             (data[algo]['elapsed'][list_length],
+#              data[algo]['comp'][list_length],
+#              data[algo]['swaps'][list_length]) = \
+#                      do_measurements(algo,
+#                                      list_length,
+#                                      iterations=ITERATIONS,
+#                                      timeit_repeat=TIMEIT_REPEAT,
+#                                      timeit_iterations=TIMEIT_ITERATIONS)
+#         print("done")
+#     print()
+#     time.sleep(1)
+#
+#     # Collect data into a dictionary consisting of equal length
+#     # lists for algorithm, list-length, elapsed, comps, and swaps
+#     perf_data = {}
+#     elapsed_data = []
+#     comps_data = []
+#     swaps_data = []
+#     algo_data = []
+#     list_length_data = []
+#     for algo in ALGORITHMS:
+#         for list_length in LIST_LENGTHS:
+#             (elapsed, comps, swaps) = algo_perf(algo, list_length)
+#             elapsed_data.append(elapsed)
+#             comps_data.append(comps)
+#             swaps_data.append(swaps)
+#             algo_data.append(algo.__name__)
+#             list_length_data.append(list_length)
+#
+#             # Append
+#             perf_data.setdefault('algorithm', []).append(algo.__name__)
+#             perf_data.setdefault('list_length', []).append(list_length)
+#             perf_data.setdefault('elapsed', []).append(elapsed)
+#             perf_data.setdefault('comps', []).append(comps)
+#             perf_data.setdefault('swaps', []).append(swaps)
+#
+#     # print(len(perf_data['elapsed']))
+#     # print(len(perf_data['comps']))
+#     # print(len(perf_data['swaps']))
+#     # quit()
+#     # Create a dataframe from the performance data
+#     df = DataFrame(perf_data,
+#                            columns=["algorithm",
+#                                     "list_length",
+#                                    "elapsed",
+#                                    "comps",
+#                                    "swaps"])
+#     # np.set_printoptions(precision=4, suppress=True)
+#
+#     # print()
+#     # print(df["algorithm"])
+#     # print("bubblesort elapsed")
+#     # print(df.loc[df.algorithm == "bubblesort", "elapsed"])
+#     # print()
+#     for algo in ALGORITHMS:
+#         print(f"algorithm: {algo.__name__}")
+#         for list_length in LIST_LENGTHS:
+#             d = df.loc[df.algorithm == algo.__name__].loc[
+#                        df.list_length == list_length,
+#                        "comps"]
+#             print(f"comps mean ({list_length}): "
+#                   f"{type(d)}")
+#             #      f"{d.mean()} ± {d.std()}")
+#         print()
+#     quit()
+#
+#     # Print results
+#     # for algo in ALGORITHMS:
+#     for algo in data:
+#         print("=" * 40, sep="")
+#         print(f"algorithm: {data[algo]['algorithm']}")
+#         print("=" * 40, sep="")
+#         for list_length in LIST_LENGTHS:
+#             print(f"list length: {list_length}")
+#             for category in categories:
+#                 print(f"\t{category}: "
+#                       f"{data[algo][category][list_length]['mean']:6.4f} "
+#                       f"± {data[algo][category][list_length]['stddev']:6.4f}")
+#             print("-" * 40, sep="")
+#         print("\n\n")
+#
+#     # Plot results
+#     do_plots(data, categories)
+#
+#     return data
+#
+#
 if __name__ == "__main__":
-    perfdata = main()
+    main()
