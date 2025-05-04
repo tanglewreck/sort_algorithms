a94c73b3e01fba2eef346aae6eb1d8cea4f35247
mier <mier@tanglewreck.mier.org>
2025-05-04 17:06:31 +0200

    Renamed variables no_comp, no_swaps -> ncomps, nswaps

--- Lektion 1/exercises/sortalgorithms/algorithms/bubblesort.py
+++ Lektion 1/exercises/sortalgorithms/algorithms/bubblesort.py
@@ -13,7 +13,7 @@ __all__ = ["bubblesort",
            "bubblesort_3"]
 
 
-def bubblesort_2(the_list: list, reverse: bool = False) -> tuple:
+def bubblesort(the_list: list, reverse: bool = False) -> tuple:
     """
     Sort a list of numbers in increasing order using bubblesort.
 
@@ -24,54 +24,54 @@ def bubblesort_2(the_list: list, reverse: bool = False) -> tuple:
     """
     list_copy = the_list.copy()
     done = False
-    no_swaps = 0
-    no_comp = 0
+    nswaps = 0
+    ncomps = 0
     while not done:
         done = True
         for index in range(len(list_copy) - 1):
-            no_comp += 1
+            ncomps += 1
             if list_copy[index] > list_copy[index + 1]:
                 done = False
-                no_swaps += 1
+                nswaps += 1
                 (list_copy[index],
                  list_copy[index + 1]) = (list_copy[index + 1],
                                           list_copy[index])
     if reverse:
-        return (list_copy[::-1], no_comp, no_swaps)
-    return (list_copy, no_comp, no_swaps)
+        return (list_copy[::-1], ncomps, nswaps)
+    return (list_copy, ncomps, nswaps)
 
 
-def bubblesort(the_list: list, reverse: bool = False) -> tuple:
+def bubblesort_2(the_list: list, reverse: bool = False) -> tuple:
     """Another bubblesort implementation (slower)"""
     list_copy = the_list.copy()
-    no_swaps = 0
-    no_comp = 0
+    nswaps = 0
+    ncomps = 0
     for _ in range(len(list_copy)):
         for index in range(len(list_copy) - 1):
-            no_comp += 1
+            ncomps += 1
             if list_copy[index] > list_copy[index + 1]:
-                no_swaps += 1
+                nswaps += 1
                 (list_copy[index],
                  list_copy[index + 1]) = (list_copy[index + 1],
                                           list_copy[index])
     if reverse:
-        return (list_copy[::-1], no_comp, no_swaps)
-    return (list_copy, no_comp, no_swaps)
+        return (list_copy[::-1], ncomps, nswaps)
+    return (list_copy, ncomps, nswaps)
 
 
 def bubblesort_3(the_list: list, reverse: bool = False) -> tuple:
     """Another bubblesort implementation"""
     list_copy = the_list.copy()
-    no_swaps = 0
-    no_comp = 0
+    nswaps = 0
+    ncomps = 0
     for index_one in range(len(list_copy)):
         for index_two in range(len(list_copy) - 1):
-            no_comp += 1
+            ncomps += 1
             if list_copy[index_one] > list_copy[index_two]:
-                no_swaps += 1
+                nswaps += 1
                 (list_copy[index_one],
                  list_copy[index_two]) = (list_copy[index_two],
                                           list_copy[index_one])
     if reverse:
-        return (list_copy[::-1], no_comp, no_swaps)
-    return (list_copy, no_comp, no_swaps)
+        return (list_copy[::-1], ncomps, nswaps)
+    return (list_copy, ncomps, nswaps)
