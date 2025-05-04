df5aef7f0605c02a64fc7684d86caba7f5d15ad9
mier <mier@tanglewreck.mier.org>
2025-05-04 11:39:41 +0200

    - Edited docstrings - Renamed variables (no_comp -> ncomps, no_swaps -> nswaps) - Added a variable index_min to keep track of the position of the smallest in the list-slice the_list[first_index:]

--- Lektion 1/exercises/sortalgorithms/algorithms/bubblesortplus.py
+++ Lektion 1/exercises/sortalgorithms/algorithms/bubblesortplus.py
@@ -1,39 +1,33 @@
 # coding: utf-8
+"""
+    NAME
+        insertionsort.py:
+    DESCRIPTION
+        Sort a list of numbers using insertion sort.
+    AUTHOR
+        mier
+    VERSION
+        2025-05-04
 
 """
-    bubblesortplus.py:
-
-    Sort a list of numbers in increasing order using
-    a modified version of bubblesort.
-
-    Returns a tuple consisting of
-        1. The sorted list
-        2. Number of comparisons made
-        3. Number of swaps made during the sorting
-"""
-
-__all__ = ["bubblesort_plus"]
-
-# pylint: disable=invalid-name
+__all__ = ["insertionsort"]
 
 
-def bubblesort_plus(the_list: list, reverse: bool = False) -> tuple:
+def insertionsort(the_list: list, reverse: bool = False) -> tuple:
     """
-    This functions sorts a list of numbers using a modified version
-    of bubblesort.
-
-    Returns a tuple consisting of
-        1. The sorted list
-        2. Number of swaps made during the sorting
-        3. Number of comparisons made
-
+        NAME
+            insertionsort
+        DESCRIPTION
+            Sorts a list of numbers using insertion sort
+        RETURNS
+            1. The sorted list (list_copy)
+            2. Number of comparisons (ncomps)
+            3. Number of swaps (nswaps)
     """
-    # Store the length of the list in a variable, for easy access
-    list_len: int = len(the_list)
 
     # We count the number of comparisons and swaps made
-    no_comp = 0
-    no_swaps = 0
+    ncomps = 0
+    nswaps = 0
 
     # Make a copy of the list and sort this instead of the original
     # so we can compare input and output.
@@ -52,24 +46,34 @@ def bubblesort_plus(the_list: list, reverse: bool = False) -> tuple:
     # the beginning of the list are all in their correct position, so we can
     # ignore those elements as we progress further down the list.
 
+    # Store the length of the list for easy access later
     list_len = len(list_copy)
     # Forward sort
-    # for index_one in range(0, list_len):  # outer loop
-    # for index_one, _ in enumerate(list_copy):  # outer loop
     for index_one in range(list_len):  # outer loop
         # If no swaps are made, we're done
         done = True
         for index_two in range(index_one + 1, list_len):  # inner loop
-            no_comp += 1
+            # Find the position of the smalles element of
+            # list_copy[index_one:], then move that element to
+            # the front of the list
+            # At first, assume that the first element is the largest
+            index_min = index_one
+            ncomps += 1
             if list_copy[index_one] > list_copy[index_two]:
-                # Make the swap
+                index_min = index_two
+                # Move the smaller element towards the front of
+                # the list
                 (list_copy[index_one],
                  list_copy[index_two]) = (list_copy[index_two],
                                           list_copy[index_one])
-                no_swaps += 1
+                nswaps += 1
                 done = False
+            if __debug__:
+                print(f"Smallest at position {index_min} ({the_list[index_min]})")
         if done:
             break
+    # Reverse sort
     if reverse:
-        return (list_copy[::-1], no_comp, no_swaps)
-    return (list_copy, no_comp, no_swaps)
+        return (list_copy[::-1], ncomps, nswaps)
+    # Forward sort
+    return (list_copy, ncomps, nswaps)
