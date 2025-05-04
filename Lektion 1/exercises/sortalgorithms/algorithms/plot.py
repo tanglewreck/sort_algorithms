bb5ae4507de6ff5071522df8273b0b7a6d891999
mier <mier@tanglewreck.mier.org>
2025-05-04 10:31:25 +0200

    Merged data plottin code into one function, plot_data()

--- Lektion 1/exercises/sortalgorithms/algorithms/plot.py
+++ Lektion 1/exercises/sortalgorithms/algorithms/plot.py
@@ -7,8 +7,10 @@
     2025-04-25
 """
 
-#
-# import numpy as np
+# pylint: disable=import-error, unused-import
+import ipdb
+# pylint: enable=import-error, unused-import
+
 import matplotlib.pyplot as plt
 from pandas import DataFrame
 
@@ -16,56 +18,46 @@ from algorithms.defaults import ALGORITHMS
 from algorithms.defaults import FIG_DIM, FIG_DPI
 from algorithms.defaults import LIST_LENGTHS
 
-__all__ = ["plot_elapsed"]
+__all__ = ["plot_data"]
 
+COLUMNS = ("t", "comps", "swaps")
 
-def plot_elapsed(data: DataFrame) -> None:
+def plot_data(data: DataFrame,
+              columns: tuple = COLUMNS) -> None:
     """
         NAME
-            plot_elapsed
+            plot_data
         DESCRIPTION
             Plot performance data.
     """
-    plt.plot(LIST_LENGTHS,
-             [data.loc[data.length==L].means
-              for L in LIST_LENGTHS])
-    plt.legend([algo.__name__ for algo in ALGORITHMS])
-    plt.ylabel("elapsed (ms)")
-    plt.xlabel("list length")
-    plt.show()
-
-
-def do_plots_old(data: dict, categories: list) -> None:
-    """
-    Plot performance data:
-        - execution time (elapsed)
-        - number of comparisons
-        - number of swaps
-    """
-
-    # Create an array (list) of means, one for
-    # each algorithm.
-    plot_data = {}
-    for algorithm in ALGORITHMS:
-        plot_data[algorithm] = {}
-        for category in categories:
-            plot_data[algorithm][category] = []
-            for list_length in LIST_LENGTHS:
-                plot_data[algorithm][category].append(
-                        data[algorithm][category][list_length]['mean'])
-
-    # Plot results (means)
-    fig, axs = plt.subplots(nrows=1, ncols=3,
-                            num="performance", sharey=False)
-    for k, ax in enumerate(axs):
+    # Start plotting: Create three subplots, all in one
+    # row, for 't' (execution time), 'comps' (comparisons),
+    # and 'swaps'.
+    ncols = len(columns)
+    fig, axs = plt.subplots(nrows=1, ncols=ncols,
+                            sharex=True, sharey=False,
+                            num="Performance plots")
+    # Handle the case of only one column to plot separatly:
+    if len(columns) == 1:
+        column = columns[0]
         for algo in ALGORITHMS:
-            ax.plot(LIST_LENGTHS,
-                    plot_data[algo][categories[k]],
-                    label=algo.__name__)
-        ax.set_xlabel('list length')
-        ax.set_ylabel(categories[k])
-        ax.legend()
-
+            axs.plot(LIST_LENGTHS,
+                     data.loc[(data.algorithm == algo.__name__)][column],
+                     label=algo.__name__)
+        axs.set_xlabel('length')
+        axs.set_ylabel(column)
+        axs.legend()
+    # For more than one column:
+    else:
+        for k, ax in enumerate(axs):
+            column = columns[k]
+            for algo in ALGORITHMS:
+                ax.plot(LIST_LENGTHS,
+                        data.loc[(data.algorithm == algo.__name__)][column],
+                        label=algo.__name__)
+            ax.set_xlabel('length')
+            ax.set_ylabel(column)
+            ax.legend()
     fig.dpi = FIG_DPI
     fig.set(figwidth=FIG_DIM[0], figheight=FIG_DIM[1])
     plt.show()
