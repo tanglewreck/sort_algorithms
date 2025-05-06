134c9ca13b1a183b2e3e8b01a8eb453969b609f0
mier <mier@tanglewreck.mier.org>
2025-05-06 12:04:29 +0200

    __init__.py: Renamed CATEGORIES --> COLUMNS (plus renamed elapsed --> t), MIN --> LOWER, MAX --> UPPER. Added LENGTH_DEFAULT. Updated LIST_LENGTHS

--- Lektion 1/exercises/sortalgorithms/algorithms/defaults.py
+++ Lektion 1/exercises/sortalgorithms/algorithms/defaults.py
@@ -22,41 +22,48 @@ from . insertionsort import insertionsort3
 # pylint: enable=unused-import
 
 __all__ = ["ALGORITHMS"]
-__all__ += ["CATEGORIES"]
+__all__ += ["COLUMNS"]
 __all__ += ["FIG_DIM", "FIG_DPI"]
 __all__ += ["ITERATIONS"]
-__all__ += ["TIMEIT_ITERATIONS", "TIMEIT_REPEAT"]
-__all__ += ["LIST_LENGTH", "LIST_LENGTHS", "MIN", "MAX"]
+__all__ += ["LENGTH_DEFAULT", "LIST_LENGTHS"]
+__all__ += ["LIST_LENGTHS_2"]
+__all__ += ["LOWER", "UPPER"]
 __all__ += ["SAVEPATH"]
+__all__ += ["TIMEIT_ITERATIONS", "TIMEIT_REPEAT"]
 
 # pylint: disable=wildcard-import, unused-wildcard-import, unused-import
 
-# ALGORITHMS = [bubblesort, bubblesort_2, insertionsort, insertionsort2 ]
-ALGORITHMS = [bubblesort, insertionsort, insertionsort2, insertionsort3]
-CATEGORIES = ['elapsed', 'comp', 'swaps']
+ALGORITHMS = (bubblesort, insertionsort, insertionsort2, insertionsort3)
+COLUMNS = ('t', 'comps', 'swaps')
+LENGTH_DEFAULT = 10
 FIG_DIM = (21, 8)
 FIG_DPI = 150
-# List-lengths to test:
-# LIST_LENGTHS = np.array(list(range(2,100)))
-# LIST_LENGTHS = np.array(list(range(10, 100, 10)))
-# LIST_LENGTHS = np.array(list(range(2, 101, 4)))
+# List-lenghts examples:
+# LIST_LENGTHS = range(2, 100)
+# LIST_LENGTHS = np.arange(2,100)
+# LIST_LENGTHS = np.arange(10, 100, 10)
 # LIST_LENGTHS = np.append(LIST_LENGTHS, np.arange(100, 301, 50))
 # LIST_LENGTHS = np.arange(100, 201, 25)
-# LIST_LENGTHS = range(10, 110, 10)
-# LIST_LENGTHS = np.array(list(range(2,10)) + list(range(10, 100, 10)))
-# LIST_LENGTHS = np.array(list(range(10, 100, 10)) +
-#                        list(range(100, 200, 50)) +
-#                        list(range(200, 600, 100)) +
-#                        list(range(600, 1200, 200)) +
-#                        [1500, 2000]
-#                        )
-# LIST_LENGTHS = np.array(list(range(10, 100, 10)))
-LIST_LENGTHS = np.array([10, 25, 50, 100, 150, 200, 250, 300])
+# LIST_LENGTHS = np.append(np.arange(2,10) + np.arange(10, 100, 10))
+# A somewhat complex case...
+LIST_LENGTHS_2 = np.append(np.arange(10, 100, 10),
+                         np.append(
+                             np.append(
+                                 np.append(np.arange(100, 200, 50),
+                                           np.arange(200, 600, 100)),
+                                 np.arange(600, 1200, 200)),
+                             np.array([1500, 2000])))
+# --> array([10, 20, 30, 40, 50, 60, 70, 80, 90, 100,
+#            150, 200, 300, 400, 500, 600, 800, 1000,
+#            1500, 2000])
+#
+# LIST_LENGTHS = np.array([10, 25, 50, 100, 150, 200, 250, 300])
+LIST_LENGTHS = np.arange(10, 100, 10)
 # Min/max sizes of a random number
-MIN, MAX = 1, 100
+LOWER, UPPER = 1, 100
 # Number of iterations while measuring performance (comparisons, swaps)
 # ITERATIONS = 200
-ITERATIONS = 2
+ITERATIONS = 10
 # Number of iterations using the timeite module
 # TIMEIT_ITERATIONS = 20
 # TIMEIT_REPEAT = 20
