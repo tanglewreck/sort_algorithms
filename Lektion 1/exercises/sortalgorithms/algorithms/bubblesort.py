3a5735e21f98b8b00c5c20ff9f679477c899c5bb
mier <mier@tanglewreck.mier.org>
2025-05-09 10:19:46 +0200

    bubblesort.py: - Renamed variable 'the_list' --> 'arr' -Renamed variable 'list_copy' --> 'arr_copy' -Updated docstrings

--- Lektion 1/exercises/sortalgorithms/algorithms/bubblesort.py
+++ Lektion 1/exercises/sortalgorithms/algorithms/bubblesort.py
@@ -1,9 +1,16 @@
 # coding: utf-8
 """
-    bubblesort.py
+    NAME
+        bubblesort
 
-    Run with '-O' to get rid of debug messages:
-    python -O bubblesort.py
+    DESCRIPTION
+        Various implementations of the bubble sort algorithm.
+
+    DATE
+        2025-04-01
+
+    REFERENCES
+        Wikipedia: https://en.wikipedia.org/wiki/bubblesort
 
 """
 # pylint: disable=consider-using-enumerate
@@ -15,156 +22,236 @@ __all__ = ["bubblesort",
            "bubblesort3",
            "bubblesort3_nocopy"]
 
+# xpylint: disable=import-error
+# xpylint: disable=unused-import
+# import ipdb
+# xpylint: enable=import-error
+# xpylint: enable=unused-import
+import numpy as np
 
-def bubblesort(the_list: list, reverse: bool = False) -> tuple:
-    """Vanilla bubblesort"""
-    list_copy = the_list.copy()
+def bubblesort(arr: np.ndarray, reverse: bool = False) -> tuple:
+    """
+        Vanilla bubble sort (https://en.wikipedia.org/wiki/bubblesort).
+
+        Parameters
+        _________
+        arr : array_like
+              Array to be sorted.
+        reverse : bool, optional
+                  Defaults to False, in which case the array is
+                  sorted in ascending order.
+
+        Returns
+        -------
+        arr_copy, ncomps, nswaps 
+            arr_copy : array_like, sorted copy of 'arr'
+            ncomps : int, number of comparisons made by the sorting algorithm.
+            nswaps : int, number of swaps made by the sorting algorithm.
+    """
+    arr_copy = arr.copy()
     nswaps = 0
     ncomps = 0
-    for _ in range(len(list_copy)):
-        for index in range(len(list_copy) - 1):
+    for _ in range(len(arr_copy)):
+        for index in range(len(arr_copy) - 1):
             ncomps += 1
-            if list_copy[index] > list_copy[index + 1]:
+            if arr_copy[index] > arr_copy[index + 1]:
                 nswaps += 1
-                (list_copy[index],
-                 list_copy[index + 1]) = (list_copy[index + 1],
-                                          list_copy[index])
+                (arr_copy[index],
+                 arr_copy[index + 1]) = (arr_copy[index + 1],
+                                          arr_copy[index])
     if reverse:
-        return (list_copy[::-1], ncomps, nswaps)
-    return (list_copy, ncomps, nswaps)
+        return (arr_copy[::-1], ncomps, nswaps)
+    return (arr_copy, ncomps, nswaps)
 
 
-def bubblesort_nocopy(the_list: list, reverse: bool = False) -> tuple:
-    """Vanilla bubblesort"""
+def bubblesort_nocopy(arr: np.ndarray, reverse: bool = False) -> tuple:
+    """
+        Vanilla bubble sort (https://en.wikipedia.org/wiki/bubblesort).
+
+        Sorts 'arr' inline (no copy of 'arr' made).
+
+        Parameters
+        _________
+        arr : array_like
+              Array to be sorted.
+        reverse : bool, optional
+                  Defaults to False, in which case the array is
+                  sorted in ascending order.
+
+        Returns
+        -------
+        arr, ncomps, nswaps 
+            arr : array_like, 'arr' sorted (inline)
+            ncomps : int, number of comparisons made by the sorting algorithm.
+            nswaps : int, number of swaps made by the sorting algorithm.
+    """
     nswaps = 0
     ncomps = 0
-    for _ in range(len(the_list)):
-        for index in range(len(the_list) - 1):
+    for _ in range(len(arr)):
+        for index in range(len(arr) - 1):
             ncomps += 1
-            if the_list[index] > the_list[index + 1]:
+            if arr[index] > arr[index + 1]:
                 nswaps += 1
-                (the_list[index],
-                 the_list[index + 1]) = (the_list[index + 1],
-                                          the_list[index])
+                (arr[index],
+                 arr[index + 1]) = (arr[index + 1],
+                                          arr[index])
     if reverse:
-        return (the_list[::-1], ncomps, nswaps)
-    return (the_list, ncomps, nswaps)
+        return (arr[::-1], ncomps, nswaps)
+    return (arr, ncomps, nswaps)
 
 
-def bubblesort2(the_list: list, reverse: bool = False) -> tuple:
+def bubblesort2(arr: np.ndarray, reverse: bool = False) -> tuple:
     """
-        NAME
-            bubblesort2
-        DESCRIPTION
-            Bubblesort somewhat optimised: returns when no
-            swaps have been done.
-            Default is to sort in increasing order.
-        PARAMETERS
-            the_list : list of numbers to be sorted
-            reverse : bool
+        Optimised bubble sort (https://en.wikipedia.org/wiki/bubblesort)
 
-        RETURNS
-            Returns a tuple consisting of
-            1. The a sorted copy of the list
-            2. Number of swaps made during the sorting
-            3. Number of comparisons made
-        DATE
-            2025-04-01
+        Parameters
+        _________
+        arr : array_like
+              Array to be sorted.
+        reverse : bool, optional
+                  Defaults to False, in which case the array is
+                  sorted in ascending order.
+
+        Returns
+        -------
+        arr_copy, ncomps, nswaps 
+            arr_copy : array_like, sorted copy of 'arr'
+            ncomps : int, number of comparisons made by the sorting algorithm.
+            nswaps : int, number of swaps made by the sorting algorithm.
     """
-    list_copy = the_list.copy()
+    arr_copy = arr.copy()
     done = False
     ncomps, nswaps = 0, 0
     # Stop when done, i.e. no swaps have been made.
     # This should speed up the algorithm somewhat
-    # compared to 'standard' bubblesort.
+    # compared to 'standard' bubble sort.
     while not done:
         done = True
-        for index in range(len(list_copy) - 1):
+        for index in range(len(arr_copy) - 1):
             ncomps += 1
-            if list_copy[index] > list_copy[index + 1]:
+            if arr_copy[index] > arr_copy[index + 1]:
                 done = False
                 nswaps += 1
-                (list_copy[index],
-                 list_copy[index + 1]) = (list_copy[index + 1],
-                                          list_copy[index])
+                (arr_copy[index],
+                 arr_copy[index + 1]) = (arr_copy[index + 1],
+                                          arr_copy[index])
     if reverse:
-        return (list_copy[::-1], ncomps, nswaps)
-    return (list_copy, ncomps, nswaps)
+        return (arr_copy[::-1], ncomps, nswaps)
+    return (arr_copy, ncomps, nswaps)
 
 
-def bubblesort2_nocopy(the_list: list, reverse: bool = False) -> tuple:
+def bubblesort2_nocopy(arr: np.ndarray, reverse: bool = False) -> tuple:
     """
-        NAME
-            bubblesort2_2
-        DESCRIPTION
-            Bubblesort somewhat optimised: returns when no
-            swaps have been done.
-           Default is to sort in increasing order.
-           Same as bubblesort2() except no copy of the list
-            is made.
-        PARAMETERS
-            the_list : list of numbers to be sorted
-            reverse : bool
+        Optimised bubble sort (https://en.wikipedia.org/wiki/bubblesort).
+        Sorts 'arr' inline.
 
-        RETURNS
-            Returns a tuple consisting of
-            1. The a sorted copy of the list
-            2. Number of swaps made during the sorting
-            3. Number of comparisons made
-        DATE
-            2025-04-01
+        Parameters
+        _________
+        arr : array_like
+              Array to be sorted.
+        reverse : bool, optional
+                  Defaults to False, in which case the array is
+                  sorted in ascending order.
+
+        Returns
+        -------
+        arr, ncomps, nswaps 
+            arr : array_like, 'arr' sorted (inline)
+            ncomps : int, number of comparisons made by the sorting algorithm.
+            nswaps : int, number of swaps made by the sorting algorithm.
     """
     done = False
     ncomps, nswaps = 0, 0
     # Stop when done, i.e. no swaps have been made.
     # This should speed up the algorithm somewhat
-    # compared to 'standard' bubblesort.
+    # compared to 'standard' bubble sort.
     while not done:
         done = True
-        for index in range(len(the_list) - 1):
+        for index in range(len(arr) - 1):
             ncomps += 1
-            if the_list[index] > the_list[index + 1]:
+            if arr[index] > arr[index + 1]:
                 done = False
                 nswaps += 1
-                (the_list[index],
-                 the_list[index + 1]) = (the_list[index + 1],
-                                          the_list[index])
+                (arr[index],
+                 arr[index + 1]) = (arr[index + 1],
+                                          arr[index])
     if reverse:
-        return (the_list[::-1], ncomps, nswaps)
-    return (the_list, ncomps, nswaps)
+        return (arr[::-1], ncomps, nswaps)
+    return (arr, ncomps, nswaps)
 
 
-def bubblesort3(the_list: list, reverse: bool = False) -> tuple:
-    """Another bubblesort implementation, or rather insertionsort(?)..."""
-    list_copy = the_list.copy()
+def bubblesort3(arr: np.ndarray, reverse: bool = False) -> tuple:
+    """
+        Optimised bubble sort* (https://en.wikipedia.org/wiki/bubblesort).
+
+        *This implementation is rather more like the insertion sort 
+         algorithm (https://en.wikipedia.org/wiki/insertionsort).
+
+        Parameters
+        _________
+        arr : array_like
+              Array to be sorted.
+        reverse : bool, optional
+                  Defaults to False, in which case the array is
+                  sorted in ascending order.
+
+        Returns
+        -------
+        arr, ncomps, nswaps 
+            arr : array_like, 'arr' sorted (inline)
+            ncomps : int, number of comparisons made by the sorting algorithm.
+            nswaps : int, number of swaps made by the sorting algorithm.
+    """
+    arr_copy = arr.copy()
     nswaps = 0
     ncomps = 0
-    for index_one in range(len(list_copy)):
-        for index_two in range(len(list_copy) - 1):
+    for index_one in range(len(arr_copy)):
+        for index_two in range(len(arr_copy) - 1):
             ncomps += 1
-            if list_copy[index_one] < list_copy[index_two]:
+            if arr_copy[index_one] < arr_copy[index_two]:
                 nswaps += 1
-                (list_copy[index_one],
-                 list_copy[index_two]) = (list_copy[index_two],
-                                          list_copy[index_one])
+                (arr_copy[index_one],
+                 arr_copy[index_two]) = (arr_copy[index_two],
+                                          arr_copy[index_one])
     if reverse:
-        return (list_copy[::-1], ncomps, nswaps)
-    return (list_copy, ncomps, nswaps)
+        return (arr_copy[::-1], ncomps, nswaps)
+    return (arr_copy, ncomps, nswaps)
 
 
-def bubblesort3_nocopy(the_list: list, reverse: bool = False) -> tuple:
-    """Another bubblesort implementation, or rather insertionsort(?)...
-       Same as bubblesort3 but no copy is made """
+def bubblesort3_nocopy(arr: np.ndarray, reverse: bool = False) -> tuple:
+    """
+        Optimised bubble sort* (https://en.wikipedia.org/wiki/bubblesort).
+
+        Sorts 'arr' inline (no copy of 'arr' is made).
+
+        *This implementation is rather more like the insertion sort 
+         algorithm (https://en.wikipedia.org/wiki/insertionsort).
+
+        Parameters
+        _________
+        arr : array_like
+              Array to be sorte.
+        reverse : bool, optional
+                  Defaults to False, in which case the array is
+                  sorted in ascending order.
+
+        Returns
+        -------
+        arr, ncomps, nswaps 
+            arr : array_like, 'arr' sorted (inline)
+            ncomps : int, number of comparisons made by the sorting algorithm.
+            nswaps : int, number of swaps made by the sorting algorithm.
+    """
     nswaps = 0
     ncomps = 0
-    for index_one in range(len(the_list)):
-        for index_two in range(len(the_list) - 1):
+    for index_one in range(len(arr)):
+        for index_two in range(len(arr) - 1):
             ncomps += 1
-            if the_list[index_one] < the_list[index_two]:
+            if arr[index_one] < arr[index_two]:
                 nswaps += 1
-                (the_list[index_one],
-                 the_list[index_two]) = (the_list[index_two],
-                                          the_list[index_one])
+                (arr[index_one],
+                 arr[index_two]) = (arr[index_two],
+                                          arr[index_one])
     if reverse:
-        return (the_list[::-1], ncomps, nswaps)
-    return (the_list, ncomps, nswaps)
+        return (arr[::-1], ncomps, nswaps)
+    return (arr, ncomps, nswaps)
