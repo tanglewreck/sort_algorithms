960d472a1ab30ed466cb0634d53cd42aa3efaa36
mier <mier@tanglewreck.mier.org>
2025-05-16 19:30:31 +0200

    algorithms/defaults.py: Rearranged stuff

--- Lektion 1/exercises/sortalgorithms/algorithms/defaults.py
+++ Lektion 1/exercises/sortalgorithms/algorithms/defaults.py
@@ -44,6 +44,45 @@ LENGTH_DEFAULT = 10
 FIG_DIM = (30, 15)
 FIG_DPI = 100
 #
+COLUMNS = ('t', 'comps', 'swaps')
+# Min/max sizes of a random number
+LOWER, UPPER = 1, 10_000
+LOWER, UPPER = 1, 100
+# Number of lists to pregenerate.
+# This number sets an upper limit to ITERATIONS.
+NPREGEN = 10_000
+# The length of the pregenerated lists
+MAXLENGTH = 20_000
+#
+# List-lenghts examples:
+# LIST_LENGTHS = np.arange(10, 100, 10)
+# LIST_LENGTHS = np.append(LIST_LENGTHS, np.arange(100, 301, 50))
+# LIST_LENGTHS = np.append(np.arange(2,10) + np.arange(10, 100, 10))
+# A somewhat complex case...
+LIST_LENGTHS_2 = np.append(np.arange(10, 100, 10),
+                           np.append(np.append(
+                                   np.append(np.arange(100, 200, 50),
+                                             np.arange(200, 600, 100)),
+                                   np.arange(600, 1200, 200)),
+                               np.array([1500, 2000])))
+# --> array([10, 20, 30, 40, 50, 60, 70, 80, 90, 100,
+#            150, 200, 300, 400, 500, 600, 800, 1000,
+#            1500, 2000])
+LIST_LENGTHS = np.append(np.arange(10, 110, 10),
+                         np.array([150, 200, 250, 300, 350, 400, 450, 500, 750,
+                                   1000, 1200]))
+LIST_LENGTHS = np.array([1000, 2000, 3000, 4000, 5000])
+LIST_LENGTHS = np.array([50, 100, 200, 500, 750])
+LIST_LENGTHS = np.array([1_000, 2_000, 3_000, 5_000])
+LIST_LENGTHS = np.array([100, 500, 1_000, 5_000, 10_000])
+LIST_LENGTHS = np.array([10, 20, 30 , 40, 50, 60, 70, 80,
+                         90, 100, 200, 300, 400, 500, 750,
+                         1_000, 2_000, 5_000, 10_000])
+LIST_LENGTHS = np.array([50, 100, 200, 500, 750, 1000, 2000])
+# Number of iterations (=number of arrays to sort) during data-collection.
+ITERATIONS = 2
+ITERATIONS = 5
+ITERATIONS = 10
 # ALGORITHMS
 BS1 = (bubblesort, )
 BS2 = (bubblesort2, )
@@ -69,48 +108,4 @@ ALGORITHMS = IS3 + ISW
 ALGORITHMS = IS3 + QS
 ALGORITHMS = BS + IS + ISW
 ALGORITHMS = IS3 + QS
-ALGORITHMS = IS3 + QS3
-#
-COLUMNS = ('t', 'comps', 'swaps')
-
-# Number of iterations (=number of arrays to sort) during data-collection.
-ITERATIONS = 10
-ITERATIONS = 5
-ITERATIONS = 2
-
-# Min/max sizes of a random number
-LOWER, UPPER = 1, 10_000
-LOWER, UPPER = 1, 100
-
-# Number of lists to pregenerate.
-# This number sets an upper limit to ITERATIONS.
-NPREGEN = 10_000
-# The length of the pregenerated lists
-MAXLENGTH = 20_000
-
-# List-lenghts examples:
-# LIST_LENGTHS = np.arange(10, 100, 10)
-# LIST_LENGTHS = np.append(LIST_LENGTHS, np.arange(100, 301, 50))
-# LIST_LENGTHS = np.append(np.arange(2,10) + np.arange(10, 100, 10))
-# A somewhat complex case...
-LIST_LENGTHS_2 = np.append(np.arange(10, 100, 10),
-                           np.append(
-                               np.append(
-                                   np.append(np.arange(100, 200, 50),
-                                             np.arange(200, 600, 100)),
-                                   np.arange(600, 1200, 200)),
-                               np.array([1500, 2000])))
-# --> array([10, 20, 30, 40, 50, 60, 70, 80, 90, 100,
-#            150, 200, 300, 400, 500, 600, 800, 1000,
-#            1500, 2000])
-LIST_LENGTHS = np.append(np.arange(10, 110, 10),
-                         np.array([150, 200, 250, 300, 350, 400, 450, 500, 750,
-                                   1000, 1200]))
-LIST_LENGTHS = np.array([50, 100, 200, 500, 750])
-LIST_LENGTHS = np.array([1000, 2000, 3000, 4000, 5000])
-LIST_LENGTHS = np.array([50, 100, 200, 500, 750])
-LIST_LENGTHS = np.array([1_000, 2_000, 3_000, 5_000])
-LIST_LENGTHS = np.array([100, 500, 1_000, 5_000, 10_000])
-LIST_LENGTHS = np.array([10, 20, 30 , 40, 50, 60, 70, 80,
-                         90, 100, 200, 300, 400, 500, 750,
-                         1_000, 2_000, 5_000, 10_000])
+ALGORITHMS = IS3 + QS2 + QS3
