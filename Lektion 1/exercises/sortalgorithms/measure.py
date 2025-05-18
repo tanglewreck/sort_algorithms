775e0184497d9e832b22ad1a6f78eb7c9411a06e
mier <mier@tanglewreck.mier.org>
2025-05-18 22:50:58 +0200

    measure.py: stuff...

--- Lektion 1/exercises/sortalgorithms/measure.py
+++ Lektion 1/exercises/sortalgorithms/measure.py
@@ -206,10 +206,10 @@ def main_function() -> None:
     #            20_000, 30_000,
     #            40_000]:
     # for ll in [100, 250, 500, 750, 1000]:
-    nlists = 2
-    ll_lower = 1_000
-    ll_upper = 2_000
-    ll_step = 100
+    nlists = 5
+    ll_lower = 10_000
+    ll_upper = 30_000
+    ll_step = 1000
     ll_range = range(ll_lower, ll_upper + 1, ll_step)
     data = genarr(size=(nlists, ll_upper))
     for ll in ll_range:
